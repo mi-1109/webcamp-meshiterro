819d7f44eb55a4fce7ed30b465fd530775a5c9d5
mi-1109 <meisuzuki.sophia@gmail.com>
2021-02-27 04:38:03 +0000

    [Start] 4-4

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,5 @@
 class PostImage < ApplicationRecord
   belongs_to :user #associationの設定
   attachment :image #画像アップ用のメソッド(attachment)を追加してフィールド名に(image)を追加（refile使用上のルール）
+  has_many :post_comments, dependent: :destroy
 end
